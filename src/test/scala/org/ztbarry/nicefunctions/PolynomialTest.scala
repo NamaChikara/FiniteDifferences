2adc198968b78df93f185a607a74df35d0478f17
zack-barry-G5 <zack.barry@getg5.com>
2019-11-25 15:07:31 -0800

    progress on Polynomial tests

--- src/test/scala/org/ztbarry/nicefunctions/PolynomialTest.scala
+++ src/test/scala/org/ztbarry/nicefunctions/PolynomialTest.scala
@@ -2,9 +2,36 @@ package org.ztbarry.nicefunctions
 
 class PolynomialTest extends org.scalatest.FunSuite {
 
-  test("Creation via array") {
+  test("Constant polynomial evaluation.") {
     val g = new Polynomial(Vector[Double](1.0))
     assert(g.eval(1.0) === 1.0)
+  }
+
+  test("Quadratic polynomial evaluation.") {
+    val g = new Polynomial(Vector[Double](0.0, 0.0, 2.0))
+    assert(g.eval(3.0) == 18.0)
+  }
+
+  test("toString formatted correctly.") {
+    val g = new Polynomial(Vector[Double](1.0, 2.0))
+    assert(g.toString == "1.0x^0 + 2.0x^1")
+  }
+
+  test("Return polynomial function definition.") {
+    val g = new Polynomial(Vector[Double](1.0, 2.0))
+    val fun: Double => Double = g.fun
+    assert(fun(2.0) == 5.0)
+  }
+
+  test("Evaluate multiple inputs") {
+    val g = new Polynomial(Vector[Double](1.0, 0.0, 0.0, 1.0))
+    assert(g.eval(Vector[Double](1.0, 2.0, 1.0)) == Vector[Double](2.0, 9.0, 2.0))
+  }
+
+  test("Multiply by a constant") {
+    val g = new Polynomial(Vector[Double](1.0, 2.0))
+    val h = g * 2.0
+    assert(h.toString == "2.0x^0 + 4.0x^1")
   }
 
 }
