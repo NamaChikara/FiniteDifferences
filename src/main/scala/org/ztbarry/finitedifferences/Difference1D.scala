323cf1a5362ce687a6effdc079af515b2a0a4e00
zack-barry-G5 <zack.barry@getg5.com>
2019-11-30 16:18:23 -0800

    added method to approximate convergence order

--- src/main/scala/org/ztbarry/finitedifferences/Difference1D.scala
+++ src/main/scala/org/ztbarry/finitedifferences/Difference1D.scala
@@ -1,11 +1,30 @@
 package org.ztbarry.finitedifferences
 
+import scala.math.log10
+
 trait Difference1D extends Mesh1D {
 
   def fun: Double => Double
 
-  def getMeshValues(h: Int): Vector[Double] = {
-    mesh(h).map(fun)
+  def meshIntegral(n: Int): Option[Double]
+
+  private val log2 = (x: Double) => log10(x)/log10(2.0)
+
+  def getMeshValues(n: Int): Vector[Double] = {
+    mesh(n).map(fun)
   }
 
+  def approxConvergenceOrder(n: Int): Option[Double] = {
+
+    val nApprox = meshIntegral(n)
+    val n2Approx = meshIntegral(n * 2)
+    val n4Approx = meshIntegral(n * 4)
+
+    if (nApprox.isEmpty | n2Approx.isEmpty | n4Approx.isEmpty) {
+      None
+    } else {
+      Some(log2((nApprox.get - n2Approx.get) / (n2Approx.get - n4Approx.get)))
+    }
+
+  }
 }
