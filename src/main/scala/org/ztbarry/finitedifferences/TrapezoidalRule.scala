d78f74c869d588b9ff90c34269da4b72a5164198
zack-barry-G5 <zack.barry@getg5.com>
2019-11-30 16:18:07 -0800

    rename function

--- src/main/scala/org/ztbarry/finitedifferences/TrapezoidalRule.scala
+++ src/main/scala/org/ztbarry/finitedifferences/TrapezoidalRule.scala
@@ -5,17 +5,17 @@ class TrapezoidalRule(val fun: Double => Double, val a: Double, val b: Double)
 
   require(a < b)
 
-  def approx(h: Int): Option[Double] = {
+  def meshIntegral(n: Int): Option[Double] = {
 
-    if (h <= 0) {
+    if (n <= 0) {
       None
     } else {
-      val funOnMesh = getMeshValues(h)
+      val funOnMesh = getMeshValues(n)
       val approx = funOnMesh
         .sliding(2)
         .map(_.sum / 2)
         .sum
-      Some(approx * (b - a) / h)
+      Some(approx * (b - a) / n)
     }
 
   }
