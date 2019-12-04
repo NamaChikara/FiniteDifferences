10d7ad8939fd41aa1b659514c0d988cc4ea660a5
zack-barry-G5 <zack.barry@getg5.com>
2019-12-04 14:19:39 -0800

    tests for Mesh1D trait

--- src/test/scala/com/ztbarry/finitedifferences/Mesh1DTest.scala
+++ src/test/scala/com/ztbarry/finitedifferences/Mesh1DTest.scala
@@ -4,16 +4,19 @@ import org.scalatest.FunSuite
 
 class Mesh1DTest extends FunSuite {
 
-  test("testB") {
-
+  test("access val a") {
+    val myMesh = new { val a = 1.0; val b = 2.0 } with Mesh1D {}
+    assert(myMesh.a == 1.0)
   }
 
-  test("testA") {
-
+  test("access val b") {
+    val myMesh = new { val a = 1.0; val b = 2.0 } with Mesh1D {}
+    assert(myMesh.b == 2.0)
   }
 
-  test("testMesh") {
-
+  test("create mesh") {
+    val myMesh = new { val a = 1.0; val b = 2.0 } with Mesh1D {}
+    assert(myMesh.mesh(4) == Vector[Double](1.00, 1.25, 1.50, 1.75, 2.00))
   }
 
 }
