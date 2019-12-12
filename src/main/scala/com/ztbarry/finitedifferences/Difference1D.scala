e303e05b5ddd0e3156ce7192913e9de08809b23d
zack-barry-G5 <zack.barry@getg5.com>
2019-12-11 19:46:24 -0800

    first sketch at automatic convergence order estimator

--- src/main/scala/com/ztbarry/finitedifferences/Difference1D.scala
+++ src/main/scala/com/ztbarry/finitedifferences/Difference1D.scala
@@ -28,9 +28,9 @@ trait Difference1D extends Mesh1D {
 
   private def getDifferenceRatio(num: Double, den: Double, n: Int): Either[String, Double] = {
     if (num == 0) {
-      Left(f"Grid approximation of interval identical for n = $n and n = ${n * 2}.")
+      Left(f"Grid approximation of integral identical for n = $n and n = ${n * 2}.")
     } else if (den == 0) {
-      Left(f"Grid approximation of interval identical for n = ${n * 2} and n = ${n * 4}.")
+      Left(f"Grid approximation of integral identical for n = ${n * 2} and n = ${n * 4}.")
     } else {
       val ratio = num / den
       if (ratio < 0) {
@@ -43,7 +43,7 @@ trait Difference1D extends Mesh1D {
   // http://www.csc.kth.se/utbildning/kth/kurser/DN2255/ndiff13/ConvRate.pdf
   def approxConvergenceOrder(n: Int): Either[String, Double] = {
     val differences = for {
-      nApprox  <- approxIntegral(n * 2)
+      nApprox  <- approxIntegral(n)
       n2Approx <- approxIntegral(n * 2)
       n4Approx <- approxIntegral(n * 4)
     } yield {
@@ -54,5 +54,29 @@ trait Difference1D extends Mesh1D {
       l => Left(l),
       r => getDifferenceRatio(r(0), r(1), n)
     )
+  }
+
+  def findConvergenceOrder(nStart: Int = 10, cutoff: Double = 0.01, maxIterations: Int = 10): Either[String, Double] = {
+    if (maxIterations < 1) {
+      Left("maxIterations must be at least 1.")
+    } else {
+      val percentChange = for {
+        nApprox  <- approxConvergenceOrder(nStart)
+        n2Approx <- approxConvergenceOrder(nStart * 2)
+      } yield {
+        (nApprox - n2Approx) / nApprox
+      }
+
+      percentChange.fold(
+        l => Left(l),
+        r => {
+          if (r < cutoff) {
+            approxConvergenceOrder(nStart * 2)
+          } else {
+            findConvergenceOrder(nStart * 2, maxIterations = maxIterations - 1)
+          }
+        }
+      )
+    }
   }
 }
