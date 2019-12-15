2deb8d87bcb2e88bdbee93612b4b33703102cdfe
zack-barry-G5 <zack.barry@getg5.com>
2019-12-14 18:13:01 -0800

    make sure to divide by trapezoid widths

--- src/main/scala/com/ztbarry/finitedifferences/TrapezoidalRule.scala
+++ src/main/scala/com/ztbarry/finitedifferences/TrapezoidalRule.scala
@@ -4,10 +4,12 @@ class TrapezoidalRule(val fun: Double => Double, val a: Double, val b: Double)
   extends Difference1D {
 
   def numericalMethod(meshValues: Vector[Double]): Double = {
-    meshValues
+    val trapezoidMidpoints = meshValues
       .sliding(2)
       .map(_.sum / 2)
-      .sum
+    val trapezoidWiths: Double = (b - a) / (meshValues.length - 1)
+
+    trapezoidMidpoints.sum * trapezoidWiths
   }
 
 }
